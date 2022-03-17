76c8dc278dfb62d9cc4e0318ea8553b3a2e70fd5
Sreekanth Reddy <sreekanth.reddy@ymedialabs.com>
2022-03-17 21:14:09 +0530

    Initial commit

---
+++ FrameworkCICD/FrameworkCICD.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkCICD.h
+//  FrameworkCICD
+//
+//  Created by YML on 17/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkCICD.
+FOUNDATION_EXPORT double FrameworkCICDVersionNumber;
+
+//! Project version string for FrameworkCICD.
+FOUNDATION_EXPORT const unsigned char FrameworkCICDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkCICD/PublicHeader.h>
+
+
